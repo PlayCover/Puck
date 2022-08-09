214be5cc7de3b1b6b1a8fb3aecf9a00cee9752bc
Robin <robinpreet.us@gmail.com>
2022-08-08 20:27:56 -0500

    finalized nvram command

--- commands/help/nvram.js
+++ commands/help/nvram.js
@@ -2,7 +2,7 @@ module.exports = class nvram extends require('../base') {
     constructor(client) {
         super(client, {
             name: 'nvram', 
-            description: 'Setup Boot Arguments for logging in apps/games',
+            description: 'Setup Boot Arguments',
             options: [
                 { name: 'user', description: 'User to ping in reply', type: 'USER', required: false }
             ]
@@ -13,23 +13,24 @@ module.exports = class nvram extends require('../base') {
      * @param {import('discord.js').CommandInteraction} interaction 
      */
     async run(interaction) {
-        let user = interaction.options.getUser('user', false);
+        let user = interaction.options.getUser('user');
         
         return interaction.reply({
-            content: user ? user.toString() : null,
+            content: user ? `${user.toString()}, ${interaction.member.toString()} wanted you to see this command` : null,
+            allowedMentions: user,
             embeds: [
                 this.client.src.embed()
-                    .setTitle('How to flawlessly input your nvram boot-args:')
-                    .setAuthor({ name: 'Nvram boot-args Setup'})
-                    .setDescription(`
-                        [] - **Command + Space** on your keyboard
-                        [] - Type **Terminal**
-                        [] - Type or copy+paste the following command:
-                        \`\`\`shsudo nvram boot-args="amfi_get_out_of_my_way=0x1 ipc_control_port_options=0"\`\`\`
-                        [] - Input your password
-                        [] - If it seems like nothing happened, **that's expected** and correct
-                        [] - Restart your mac
-                        [] - **Enjoy!**
+                    .setAuthor({ name: `Requested by: ${interaction.member.nickname}`, iconURL: interaction.user.avatarURL() })
+                    .setTitle('How to input the recommended nvram boot-args:')
+                    .setDescription(`   
+                        ➤ **Command + Space** on your keyboard
+                        ➤ Type **Terminal**
+                        ➤ Type or copy+paste the following command:
+                        \`\`\`sudo nvram boot-args="amfi_get_out_of_my_way=0x1 ipc_control_port_options=0"\`\`\`
+                        ➤ Input your password
+                        ➤ If it seems like nothing happened, **that's expected** and correct
+                        ➤ Restart your mac
+                        ➤ **Enjoy!**
                     `)
             ],
             ephemeral: user ? false : true
