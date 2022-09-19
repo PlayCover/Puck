175a716ad9e8e4fe027d979b6444fae615e9808c
Roeegh <admin@roeegh.dev>
2022-09-19 14:59:34 -0500

    🌸 Fixed bot status

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,7 +4,10 @@ module.exports = {
 	name: 'messageCreate',
 	async execute(message) {
         if (message.author.bot || message.channel.type === 'DM') return;
-        if (message.content.includes(`<@${message.client.user.id}>`) || message.content.includes(`<@!${message.client.user.id}>`)) message.channel.send((message.content.replace('<@1000077833550434304>', message.author.toString())).toUpperCase());
+        
+
+
+        if (message.content.includes(`<@${message.client.user.id}>`) || message.content.includes(`<@!${message.client.user.id}>`)) message.channel.send(message.author.toString());
         if (message.content === '(\u256F\u00B0\u25A1\u00B0\uFF09\u256F\uFE35 \u253B\u2501\u253B') message.reply('\u252C\u2500\u252C \u30CE( \u309C-\u309C\u30CE)');
 
         if (message.content.startsWith(':') && message.content.endsWith(':') && message.content !== '::') {
