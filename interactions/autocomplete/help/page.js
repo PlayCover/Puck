7b5b32f27a4837ea668181fcad61a961c2cfbab9
Amir Mohammadi <amirkamangar@icloud.com>
2022-11-19 01:54:08 +0330

    AutoComplete Module for Page.json

---
+++ interactions/autocomplete/help/page.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'page',
+	async execute(interaction) {
+		if (interaction.isAutocomplete()) {
+            let focusedOption = interaction.options.getFocused();
+            let choices = require('../../../resources/pages.json').map(page => page.name);
+            let filtered = choices.filter(c => c.toLowerCase().includes(focusedOption.toLowerCase()));
+            return interaction.respond(filtered.map(c => ({ name: c, value: c })));
+        }
+	},
+};
