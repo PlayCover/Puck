37527483835e83b1a31f2f42f6cb1d1d20cfaacd
Acquite <acquitefx@gmail.com>
2022-11-18 21:38:59 +0000

    make download command take a true choice

--- interactions/slash/users/download.js
+++ interactions/slash/users/download.js
@@ -6,40 +6,38 @@ module.exports = {
 		.setDescription('Get the download link for PlayCover')
         .addStringOption(option =>
             option.setName('nightly')
-                .setDescription('Get the Nightly build download link'))
+                .setDescription('Get the Nightly build download link')
+                .addChoices(
+                {
+                    name: "true",
+                    displayName: "true",
+                    value: true
+                }
+            ))
         .addUserOption(option => option.setName('user').setDescription('User to ping in reply')),
 
 	async execute(interaction) {
-        let beta = interaction.options.getString('nightly');
-        let user = interaction.options.getUser('user');
+        const is_nightly = interaction.options.getString('nightly');
+        const user = interaction.options.getUser('user');
         
-        if (beta) {
-            return interaction.reply({
-                content: user ? `${user.toString()}, ${interaction.member.toString()} wanted you to see this command` : null,
-                allowedMentions: { users: [user ? user.id : null] },
-                embeds: [
-                    new EmbedBuilder()
-                    .setTitle(`Latest PlayCover Nightly Build`)
-                    .setDescription(`[Click here to go to download page](https://nightly.link/PlayCover/PlayCover/workflows/2.nightly_release/develop)`)
-                    .setAuthor({ name: 'Nightly.link'})
-                    .setThumbnail(interaction.guild.iconURL())
-                ],
-                ephemeral: user ? false : true
-            }).catch(error => console.error(`[ERROR]: ${error}`));
-        } else {
-            return interaction.reply({
-                content: user ? `${user.toString()}, ${interaction.member.toString()} wanted you to see this command` : null,
-                allowedMentions: { users: [user ? user.id : null] },
-                embeds: [
-                    new EmbedBuilder()
-                        .setTitle('Click here to go to PlayCover stable releases download page')
-                        .setURL('https://github.com/PlayCover/PlayCover/releases')
-                        .setAuthor({ name: 'PlayCover GitHub'})
-                        .setThumbnail(interaction.guild.iconURL())
-                        .setColor('Random')
-                ],
-                ephemeral: user ? false : true
-            }).catch(error => console.error(`[ERROR]: ${error}`));
-        }
+        return interaction.reply({
+            content: user ? `${user.toString()}, ${interaction.member.toString()} wanted you to see this command` : null,
+            allowedMentions: { users: [user ? user.id : null] },
+            embeds: [
+                new EmbedBuilder()
+                .setTitle(is_nightly 
+                    ? `Latest PlayCover Nightly Build` 
+                    : 'Click here to go to PlayCover stable releases download page')
+                .setDescription(is_nightly 
+                    ? `[Click here to go to download page](https://nightly.link/PlayCover/PlayCover/workflows/2.nightly_release/develop)`
+                    : 'https://github.com/PlayCover/PlayCover/releases')
+                .setAuthor({ name: is_nightly 
+                    ? 'Nightly.link'
+                    : 'PlayCover GitHub'})
+                .setThumbnail(interaction.guild.iconURL())
+                .setColor("Random")
+            ],
+            ephemeral: user ? false : true
+        }).catch(error => console.error(`[ERROR]: ${error}`))
     }
 };
