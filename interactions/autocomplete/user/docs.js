39ffdcf5375f3bd99688cff46afff275ea765167
Roeegh <admin@roeegh.dev>
2022-11-19 20:27:19 -0600

    🌸 Added docs command

---
+++ interactions/autocomplete/user/docs.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'docs',
+    async execute(interaction) {
+        if (interaction.isAutocomplete()) {
+            let focusedOption = interaction.options.getFocused();
+            let choices = require('../../../resources/pages.json').map(keymap => keymap.name);
+            let filtered = choices.filter(c => c.toLowerCase().includes(focusedOption.toLowerCase()));
+            return interaction.respond(filtered.map(c => ({ name: c, value: c })));
+        }
+    },
+};
